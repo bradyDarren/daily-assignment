4e4f04a3538d2b5aa5717854d34a5ea5ced952f5
Darren Brady <dcgbfinancial@gmail.com>
2024-11-01 16:25:40 +0000

    updated data

---
+++ data.py
@@ -0,0 +1,22 @@
+employees_data ={
+    "3955185": {"firstname": "Darren", "lastname": "Brady", "classification": 1},
+    "3955186": {"firstname": "Evelyn", "lastname": "Smith", "classification": 2},
+    "3955187": {"firstname": "Liam", "lastname": "Johnson", "classification": 3},
+    "3955188": {"firstname": "Olivia", "lastname": "Brown", "classification": 1},
+    "3955189": {"firstname": "Noah", "lastname": "Davis", "classification": 2},
+    "3955190": {"firstname": "Emma", "lastname": "Miller", "classification": 3},
+    "3955191": {"firstname": "James", "lastname": "Wilson", "classification": 1},
+    "3955192": {"firstname": "Ava", "lastname": "Moore", "classification": 2},
+    "3955193": {"firstname": "Benjamin", "lastname": "Taylor", "classification": 3},
+    "3955194": {"firstname": "Sophia", "lastname": "Anderson", "classification": 1},
+    "3955195": {"firstname": "Lucas", "lastname": "Thomas", "classification": 2},
+    "3955196": {"firstname": "Mia", "lastname": "Jackson", "classification": 3},
+    "3955197": {"firstname": "Henry", "lastname": "White", "classification": 1},
+    "3955198": {"firstname": "Ella", "lastname": "Harris", "classification": 2},
+    "3955199": {"firstname": "Alexander", "lastname": "Martin", "classification": 3},
+    "3955200": {"firstname": "Grace", "lastname": "Garcia", "classification": 1},
+    "3955201": {"firstname": "William", "lastname": "Martinez", "classification": 2},
+    "3955202": {"firstname": "Scarlett", "lastname": "Robinson", "classification": 3},
+    "3955203": {"firstname": "Samuel", "lastname": "Clark", "classification": 1},
+    "3955204": {"firstname": "Isabella", "lastname": "Rodriguez", "classification": 2}
+}
