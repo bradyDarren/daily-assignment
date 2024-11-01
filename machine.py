abb35b078ace8019c39ba0e0a9a95a68b8b1715c
Darren Brady <dcgbfinancial@gmail.com>
2024-11-01 22:22:08 +0000

    addition of the get_assignment_1 method

--- machine.py
+++ machine.py
@@ -1,11 +1,27 @@
-machines = {
-    "red": 2,
-    "green": 2,
-    "blue": 1,
-    "orange": 4
-}
 
-riding_machines = {
-    "claw" : 1,
-    "forklift" : 2
-}
+class DP1Machines:
+
+    machines = {
+        "red": 2,
+        "green": 2,
+        "blue": 1,
+        "orange": 4
+    }
+
+    def get_assignements_1(self):
+        # prints a current report of all of the possible DP1 assignments
+        options = ""
+        for baler in self.machines:
+            options += f"{baler}/"
+        return options
+        
+
+class DP2Machines:
+    riding_machines = {
+        "claw" : 1,
+        "forklift" : 2
+    }
+
+x = DP1Machines()
+
+print(x.get_assignements_1()[:-1])
