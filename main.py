cb56939446011be65a1b964144a1b7660571866f
Darren Brady <dcgbfinancial@gmail.com>
2024-11-01 22:22:36 +0000

    testing classes

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from data import employees_data
-from employees import Employees, Classification
-from machine import machines, riding_machines
+from employees import Employees, DP1, DP2
+from machine import DP1Machines, DP2Machines
 
-people_f_name = []
+DP1_assignments = DP1Machines.machines
 
-for emp_data in employees_data:
-    department = Employees(emp_num=emp_data["employee id"],
-                           f_name=emp_data["first name"],
-                           l_name=emp_data["last name"])
-    people_f_name.append(department.f_name)
-    print(people_f_name)
+# people_f_name = []
+
+# for emp_data in employees_data:
+#     department = Employees(emp_num=emp_data["employee id"],
+#                            f_name=emp_data["first name"],
+#                            l_name=emp_data["last name"])
+#     people_f_name.append(department.f_name)
+#     print(people_f_name)
