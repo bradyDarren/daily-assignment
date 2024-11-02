9a917fe2c3a0b9cd73e81fe060059224a4e4d8ce
Darren Brady <dcgbfinancial@gmail.com>
2024-11-02 21:36:46 +0000

    addition of the get_demand methods

--- machine.py
+++ machine.py
@@ -17,6 +17,11 @@ class DP1Machines:
             baler.append(f"{count}. {machine_name} remaining : {self.machines[machine_name]}")
         return baler
     
+    def get_demand_1(self):
+        total_demand_1 = 0
+        for value in self.machines:
+            total_demand_1 += self.machines[value]
+        return total_demand_1
 
 class DP2Machines:
 
@@ -32,6 +37,14 @@ class DP2Machines:
             count += 1
             assignments.append(f"{count}. {task} remaining : {self.riding_machines[task]}")
         return assignments
+    
+    def get_demand_2(self):
+        total_demand_2 = 0
+        for value in self.riding_machines:
+            total_demand_2 += self.riding_machines[value]
+        return total_demand_2 
 
-x = DP1Machines()
-print(x.get_machine_1())
+# x = DP1Machines()
+# print(x.get_machine_1())
+# print(DP1Machines().get_demand_1())
+# print(DP2Machines().get_demand_2())
