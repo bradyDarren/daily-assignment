a7821e76bcbcff7c443853a968af63bc986d67a7
Darren Brady <dcgbfinancial@gmail.com>
2024-11-02 22:14:53 +0000

    addition of retreive_name method

--- assignment_selection.py
+++ assignment_selection.py
@@ -1,8 +1,10 @@
 
-class assignment_selection: 
+class assignment_selection:
 
-    def __init__(self, employement_list) -> None:
+    def __init__(self, employement_list, tasks_list_1, tasks_list_2) -> None:
         self.employement_list = employement_list
+        self.tasks_list_1 = tasks_list_1
+        self.tasks_list_2 = tasks_list_2
         self.dp1_count = 0
         self.dp2_count = 0
     
