f3abd926f96cf39d55d2b54d9c6fb7c16cb7d849
Darren Brady <dcgbfinancial@gmail.com>
2024-11-02 11:03:39 +0000

    removal of additional functions

--- employees.py
+++ employees.py
@@ -6,16 +6,12 @@ class Employees:
         self.l_name = l_name
 
 class DP1(Employees):
-    def __init__(self, emp_num, f_name, l_name, level) -> None:
-        super().__init__(emp_num, f_name, l_name)
-        self.level = level
 
-    # def dp1_assignment(self, assignment, emp_class):
-    #     if emp_class in assignment:
-
-class DP2(Employees):
     def __init__(self, emp_num, f_name, l_name, level) -> None:
         super().__init__(emp_num, f_name, l_name)
         self.level = level
     
-    # def dp2_assignment(self, assignment):
+class DP2(Employees):
+    def __init__(self, emp_num, f_name, l_name, level) -> None:
+        super().__init__(emp_num, f_name, l_name)
+        self.level = level
