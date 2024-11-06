c2dbe78f1ee4fd7f653bb168338ef2141baf1fc8
Darren Brady <dcgbfinancial@gmail.com>
2024-11-06 14:25:27 +0000

    repurpose function to employees class

--- employees.py
+++ employees.py
@@ -4,14 +4,20 @@ class Employees:
         self.emp_num = emp_num
         self.f_name = f_name
         self.l_name = l_name
-
-class DP1(Employees):
-
-    def __init__(self, emp_num, f_name, l_name, level) -> None:
-        super().__init__(emp_num, f_name, l_name)
-        self.level = level
     
-class DP2(Employees):
-    def __init__(self, emp_num, f_name, l_name, level) -> None:
-        super().__init__(emp_num, f_name, l_name)
-        self.level = level
+    def retreive_name(self, emp_number, employee_list):
+        if emp_number in employee_list:
+            return f"{employee_list[emp_number]["firstname"]} {employee_list[emp_number]["lastname"]}"
+        else: 
+            return f"{emp_number} is not within the database."
+
+# class DP1(Employees):
+
+#     def __init__(self, emp_num, f_name, l_name, level) -> None:
+#         super().__init__(emp_num, f_name, l_name)
+#         self.level = level
+    
+# class DP2(Employees):
+#     def __init__(self, emp_num, f_name, l_name, level) -> None:
+#         super().__init__(emp_num, f_name, l_name)
+#         self.level = level
