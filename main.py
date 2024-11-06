0b43a791a45cf3ebe481b14480156765ba74c7b0
Darren Brady <dcgbfinancial@gmail.com>
2024-11-06 23:20:58 +0000

    addition of a list of the remaining assignments

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ from employees import Employees
 from machine import DP1Machines, DP2Machines
 from check import checks
 
+# list the types of assignments available for category one partners: 
+dp1_assignments = DP1Machines().get_machine_1()[:-1]
+print(dp1_assignments)
+
+print()
+
+# list the types of assignments available for category two partners: 
+dp2_assignments = DP2Machines().get_machine_2()[:-1]
+print(dp2_assignments)
+
 user_emp_num = checks().check_int("Please enter your PeopleSoft Number: ")
+
 
 # greeting = assignment_selection()
