e2b9b8e86361280663652883fd9318c17154b1d1
Darren Brady <dcgbfinancial@gmail.com>
2024-11-06 14:24:34 +0000

    removal of class

--- assignment_selection.py
+++ assignment_selection.py
@@ -1,15 +1,15 @@
+# removal of class. 
+# class assignment_selection:
 
-class assignment_selection:
-
-    def __init__(self, employement_list, tasks_list_1, tasks_list_2) -> None:
-        self.employement_list = employement_list
-        self.tasks_list_1 = tasks_list_1
-        self.tasks_list_2 = tasks_list_2
-        self.dp1_count = 0
-        self.dp2_count = 0
+    # def __init__(self, employement_list, tasks_list_1, tasks_list_2) -> None:
+    #     self.employement_list = employement_list
+    #     self.tasks_list_1 = tasks_list_1
+    #     self.tasks_list_2 = tasks_list_2
+    #     self.dp1_count = 0
+    #     self.dp2_count = 0
     
-    def retreive_name(self, emp_number):
-        if emp_number in self.employement_list:
-            return self.employement_list[emp_number]["firstname"],self.employement_list[emp_number]["lastname"]
-        else: 
-            return f"{emp_number} is not within the database."
+    # def retreive_name(self, emp_number, employee_list):
+    #     if emp_number in employee_list:
+    #         return f"{employee_list[emp_number]["firstname"]} {employee_list[emp_number]["lastname"]}"
+    #     else: 
+    #         return f"{emp_number} is not within the database."
