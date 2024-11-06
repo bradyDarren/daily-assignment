7f7e4184fa13668bd026769054a6eaf4912170a3
Darren Brady <dcgbfinancial@gmail.com>
2024-11-06 14:26:08 +0000

    removal of for loop function that created a list.

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 from data import employees_data
-from employees import Employees, DP1, DP2
+from employees import Employees
 from machine import DP1Machines, DP2Machines
-from assignment_selection import assignment_selection
 from check import checks
 
-employees_database = []
-
-for employee in employees_data:
-    if employees_data[employee]["classification"] == 1:
-        e_database = Employees(employee, employees_data[employee]["firstname"], employees_data[employee]["lastname"])
-        employees_database.append(e_database)
-
-# test
-# print(employees_database[0].emp_num)
+user_emp_num = checks().check_int("Please enter your PeopleSoft Number: ")
 
 # user_input = checks().check_int("Hello, Please input your Peoplesoft number : ")
 
