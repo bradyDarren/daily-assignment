744872f89a5c885d4d803001dd91b5c8ab22633e
Darren Brady <dcgbfinancial@gmail.com>
2024-11-07 12:48:40 +0000

    addition of the while loop

--- main.py
+++ main.py
@@ -5,20 +5,19 @@ from check import checks
 import random 
 
 # list the types of assignments available for category one partners: 
-dp1_assignments = DP1Machines().get_machine_1()[:-1]
-print(dp1_assignments)
-
-print()
+dp1_machines = DP1Machines()
 
 # list the types of assignments available for category two partners: 
-dp2_assignments = DP2Machines().get_machine_2()[:-1]
-print(dp2_assignments)
-
-print() 
-
-# user_emp_num = checks().check_int("Please enter your PeopleSoft Number: ")
-
+dp2_machines = DP2Machines()
 
 # random output from machines
 # rand_machine = random.choice(list(DP1Machines().machines.keys()))
 # print(rand_machine)
+
+while True:
+    
+    print(dp1_machines.message)
+    print(dp1_machines.get_machine_1())
+    print(dp2_machines. get_machine_2())
+
+    user_emp_num = checks().check_int("Please enter your PeopleSoft Number: ")
