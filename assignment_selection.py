8572da1b9ed9ddc67dcd909d12f95d3cb91e79dd
Darren Brady <dcgbfinancial@gmail.com>
2024-11-07 17:53:04 +0000

    addition of the assign_task method

--- assignment_selection.py
+++ assignment_selection.py
@@ -1,15 +1,8 @@
-# removal of class. 
-# class assignment_selection:
+import random
 
-    # def __init__(self, employement_list, tasks_list_1, tasks_list_2) -> None:
-    #     self.employement_list = employement_list
-    #     self.tasks_list_1 = tasks_list_1
-    #     self.tasks_list_2 = tasks_list_2
-    #     self.dp1_count = 0
-    #     self.dp2_count = 0
-    
-    # def retreive_name(self, emp_number, employee_list):
-    #     if emp_number in employee_list:
-    #         return f"{employee_list[emp_number]["firstname"]} {employee_list[emp_number]["lastname"]}"
-    #     else: 
-    #         return f"{emp_number} is not within the database."
+class assignment_selection: 
+
+    def assign_task(self, assign_list):
+        # generates a random assignment based on the given dict. 
+        assignment = random.choice(list(assign_list.keys()))
+        return assignment
