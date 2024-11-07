aee83234dac3d6c20353338cbc322eccf8ac7ec6
Darren Brady <dcgbfinancial@gmail.com>
2024-11-07 12:48:17 +0000

    addition of the employee_level method

--- employees.py
+++ employees.py
@@ -1,15 +1,23 @@
 
 class Employees:
 
-    def retreive_name(self, emp_list, emp_number):
-        if emp_number in emp_list:
-            print (f"{emp_list[emp_number]["firstname"]} {emp_list[emp_number]["lastname"]}")
+    dp_categories = ["dp1","dp2"]
+
+    def __init__(self, employee_list):
+        self.employee_list = employee_list
+
+    def retreive_name(self, emp_number):
+        if emp_number in self.employee_list:
+            print (f"{self.employee_list[emp_number]["firstname"]} {self.employee_list[emp_number]["lastname"]}")
         else: 
             print (f"{emp_number} is not within the database.")
     
     def remove_assigned(self, emp_list, emp_number):
         if emp_number in emp_list:
             del emp_list[emp_number]
+
+    # def employee_level(self, emp_number, emp_list):
+
 
 # class DP1(Employees):
 
