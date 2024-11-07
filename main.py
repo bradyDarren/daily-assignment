6fbfcc55f9b99155a6723e96883f71f3516c12e2
Darren Brady <dcgbfinancial@gmail.com>
2024-11-07 00:06:38 +0000

    tested the random selection of the machines for DP1

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from data import employees_data
 from employees import Employees
 from machine import DP1Machines, DP2Machines
 from check import checks
+import random 
 
 # list the types of assignments available for category one partners: 
 dp1_assignments = DP1Machines().get_machine_1()[:-1]
@@ -13,7 +14,11 @@ print()
 dp2_assignments = DP2Machines().get_machine_2()[:-1]
 print(dp2_assignments)
 
-user_emp_num = checks().check_int("Please enter your PeopleSoft Number: ")
+print() 
+
+# user_emp_num = checks().check_int("Please enter your PeopleSoft Number: ")
 
 
-# greeting = assignment_selection()
+# random output from machines
+# rand_machine = random.choice(list(DP1Machines().machines.keys()))
+# print(rand_machine)
