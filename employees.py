e59f4f4c0170c33fe857c51f7ef0765b4b2ced77
Darren Brady <dcgbfinancial@gmail.com>
2024-11-07 00:07:24 +0000

    addition of the remove_assigned function

--- employees.py
+++ employees.py
@@ -1,15 +1,15 @@
 
 class Employees:
-    def __init__(self, emp_num, f_name, l_name) -> None:
-        self.emp_num = emp_num
-        self.f_name = f_name
-        self.l_name = l_name
-    
-    def retreive_name(self, emp_number, employee_list):
-        if emp_number in employee_list:
-            return f"{employee_list[emp_number]["firstname"]} {employee_list[emp_number]["lastname"]}"
+
+    def retreive_name(self, emp_list, emp_number):
+        if emp_number in emp_list:
+            print (f"{emp_list[emp_number]["firstname"]} {emp_list[emp_number]["lastname"]}")
         else: 
-            return f"{emp_number} is not within the database."
+            print (f"{emp_number} is not within the database.")
+    
+    def remove_assigned(self, emp_list, emp_number):
+        if emp_number in emp_list:
+            del emp_list[emp_number]
 
 # class DP1(Employees):
 
@@ -21,3 +21,5 @@ class Employees:
 #     def __init__(self, emp_num, f_name, l_name, level) -> None:
 #         super().__init__(emp_num, f_name, l_name)
 #         self.level = level
+
+
