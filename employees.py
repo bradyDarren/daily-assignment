89d36e5f13d25ef7d73572c0da62dd72cb1f1152
Darren Brady <dcgbfinancial@gmail.com>
2024-11-08 16:31:47 +0000

    addition of comments to communicate the utility of methods

--- employees.py
+++ employees.py
@@ -7,22 +7,26 @@ class Employees:
         self.employee_list = employee_list
 
     def retreive_name(self, emp_number):
+        # returns first and last name of employee based on the input employee number.
         if emp_number in self.employee_list:
             return f"{self.employee_list[emp_number]["firstname"]} {self.employee_list[emp_number]["lastname"]}"
         else: 
             return (f"{emp_number} is not within the database.")
     
     def remove_assigned(self, emp_number):
+        # removes employee from the list of employees waiting to be assigned. 
         if emp_number in self.employee_list:
             del self.employee_list[emp_number]
         
     def check_user_input(self, user_emp_num):
+        # checks if user input is within the employee database.
         if user_emp_num in list(self.employee_list.keys()):
             return True
         else:
             return False
 
     def employee_level(self, partner):
+        # checks the classification level of input partner.
         if self.employee_list[partner]["classification"] == 1:
             return True
         elif self.employee_list[partner]["classification"] == 2:
